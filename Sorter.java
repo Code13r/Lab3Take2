b597a92f0598d1b59abeae45b41c05014ece3bd3
Rashed <r45hed3@gmail.com>
2023-09-25 20:41:19 +0300

    added A sort

--- Sorter.java
+++ Sorter.java
@@ -5,4 +5,7 @@ public class Sorter {
     public void selectionSort(int[] arr) {
         System.out.println("the array has been sorted using selection sort");
     }
+    public void sort(int[] arr){
+        selectionSort(arr);
+    }
 }
