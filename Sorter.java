0906690270d4ac4248e58a9c30f975e2565bb4a7
Rashed <r45hed3@gmail.com>
2023-09-25 20:45:14 +0300

    resolved fr

--- Sorter.java
+++ Sorter.java
@@ -9,15 +9,9 @@ public class Sorter {
     public void selectionSort(int[] arr) {
         System.out.println("the array has been sorted using selection sort");
     }
-<<<<<<< HEAD
+
     public void sort(int[] arr){
         selectionSort(arr);
     }
-=======
 
-    public void sort(arr){
-        insertionSort(arr);
-    }
-
->>>>>>> a8ab049bc5d7ca4288cdb5352ef637baa5ebf27c
 }
